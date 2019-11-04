a87aec337ae5adc5f611de821c5ac01e4a605f0f
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2019-11-04 15:43:20 +0900

    S3最後

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,10 @@
+set :default_env, {
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+}
+
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.2"
 
@@ -37,6 +44,19 @@ namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
+end
+
+desc 'upload secrets.yml'
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+      upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
+    end
+  end
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
 end
 
 # Default branch is :master
